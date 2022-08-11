1e507a91f5c17a15df167438c9dfa846c52d67a2
ClaytonDewey <clay@dryadmedia.com>
2022-08-11 18:15:09 -0500

    Delete unneeded files, and set up boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>I like pie</p>
+    </div>
+  );
+};
+
+export default App;
