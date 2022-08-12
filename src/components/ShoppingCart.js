a903ec08eb42db47f530c4b2ebfa97c6bf95ed73
ClaytonDewey <clay@dryadmedia.com>
2022-08-12 13:26:29 -0500

    Render items in cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const ShoppingCart = () => {
-  return <div id="cart__dropdown" className="cart__dropdown"></div>;
+const ShoppingCart = ({ itemsInCart, onClick }) => {
+  return (
+    <div id="cart__dropdown" className="cart__dropdown open">
+      {itemsInCart.map((item) => {
+        return (
+          <div key={item.prodId} className="cart__item">
+            <button
+              onClick={() => onClick(item.prodId)}
+              className="btn btn__delete"
+            >
+              -
+            </button>
+            <img
+              className="cart__img"
+              src={item.mediumImageURL}
+              alt={item.caption}
+            />
+            <span className="cart__price">
+              {item.currency}
+              {item.price}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ShoppingCart;
