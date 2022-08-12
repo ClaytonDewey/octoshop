75e7251ff2b184f74f3fba7aad1727e5c21fde2b
ClaytonDewey <clay@dryadmedia.com>
2022-08-12 13:26:00 -0500

    Move state and useEffect from Main to App

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,40 @@ import Products from "../ListJSONTest.json";
 import "./App.scss";
 
 const App = () => {
+  const [itemsInCart, setItemsInCart] = useState([]);
+
+  useEffect(() => {
+    const items = window.localStorage.getItem("cart");
+    if (items !== null) setItemsInCart(JSON.parse(items));
+  }, []);
+
+  useEffect(() => {
+    if (itemsInCart.length) {
+      window.localStorage.setItem("cart", JSON.stringify(itemsInCart));
+    }
+  }, [itemsInCart]);
+
+  const addToCart = (prodId, mediumImageURL, currency, price, caption) => {
+    const itemToAdd = {
+      prodId: prodId,
+      mediumImageURL: mediumImageURL,
+      currency: currency,
+      price: price,
+      caption: caption,
+    };
+    setItemsInCart([...itemsInCart, itemToAdd]);
+  };
+
+  const removeFromCart = (prodId) => {
+    const updatedCart = itemsInCart.filter((item) => item.prodId !== prodId);
+    console.log(updatedCart);
+    setItemsInCart(updatedCart);
+  };
+
   return (
     <>
-      <Header />
-      <Main Products={Products} />
+      <Header itemsInCart={itemsInCart} onClick={removeFromCart} />
+      <Main Products={Products} onClick={addToCart} />
       <Footer />
     </>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,50 +1,13 @@
-import React, { useState, useEffect } from "react";
-// import localStorage from "localStorage";
+import React from "react";
 
-const Main = ({ Products }) => {
+const Main = ({ Products, onClick }) => {
   const Items = Products.List;
-  const [itemsInCart, setItemsInCart] = useState([]);
-
-  // useEffect(() => {
-  //   const loadItems = () => {
-  //     setItemsInCart(JSON.parse(localStorage.getItem("cart")));
-  //   };
-
-  //   loadItems();
-  // });
-
-  // const showCart = () => {
-  //   const cartItems = JSON.parse(localStorage.getItem("cart") || []);
-  //   cartItems.map((item) => {
-  //     return `
-  //     <div class="cart__item">
-  //       <button onclick="removeFromCart(${item.prodId})" class="btn btn-delete">-</button>
-  //       <img class="cart__img" src="${item.image}" alt="${item.caption} />
-  //       <span class="cart__price">${item.currency}${item.price}</span>
-  //     </div>
-  //   `;
-  //   });
-  // };
-
-  const addToCart = (prodId, mediumImageURL, currency, price, caption) => {
-    console.log(prodId, mediumImageURL, currency, price, caption);
-    const itemToAdd = {
-      prodId: prodId,
-      mediumImageURL: mediumImageURL,
-      currency: currency,
-      price: price,
-      caption: caption,
-    };
-    setItemsInCart([...itemsInCart, itemToAdd]);
-    console.log(itemsInCart);
-    localStorage.setItem("cart", JSON.stringify(itemsInCart));
-  };
 
   return (
     <>
-      <div id="loading" className="loading">
+      {/* <div id="loading" className="loading">
         Loading...
-      </div>
+      </div> */}
       <main id="items" className="container">
         {Items.map((item) => {
           const {
@@ -70,7 +33,7 @@ const Main = ({ Products }) => {
               </p>
               <button
                 onClick={() =>
-                  addToCart(prodId, mediumImageURL, currency, price, caption)
+                  onClick(prodId, mediumImageURL, currency, price, caption)
                 }
                 className="btn btn__primary"
               >
