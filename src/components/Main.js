e7a40073108617752d52b77b693563e0f3068e4b
ClaytonDewey <clay@dryadmedia.com>
2022-08-13 13:01:14 -0500

    Set up filtering option

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,10 @@ const Main = ({ Products, onClick }) => {
   const [isOpen, setOpen] = useState("false");
   const [selectedItem, setSelectedItem] = useState({});
 
+  const sortItems = (e) => {
+    console.log(e.target.value);
+  };
+
   const renderContent = () => {
     return (
       <>
@@ -13,7 +17,7 @@ const Main = ({ Products, onClick }) => {
           <img src={selectedItem.mediumImageURL} alt={selectedItem.caption} />
         </div>
         <div className="modal__content-text">
-          <p>{selectedItem.description}</p>
+          <p dangerouslySetInnerHTML={{ __html: selectedItem.description }}></p>
           <p>{selectedItem.brand}</p>
           <p>Item: {selectedItem.prodId}</p>
           <p>
@@ -28,7 +32,21 @@ const Main = ({ Products, onClick }) => {
   const renderActions = () => {
     return (
       <>
-        <button className="btn btn__primary">Add to Cart</button>
+        <button
+          onClick={() => {
+            setOpen("false");
+            onClick(
+              selectedItem.prodId,
+              selectedItem.mediumImageURL,
+              selectedItem.currency,
+              selectedItem.price,
+              selectedItem.caption
+            );
+          }}
+          className="btn btn__primary"
+        >
+          Add to Cart
+        </button>
         <button onClick={handleToggle} className="btn btn__cancel">
           Close
         </button>
@@ -45,6 +63,37 @@ const Main = ({ Products, onClick }) => {
       {/* <div id="loading" className="loading">
         Loading...
       </div> */}
+      <div className="filter">
+        <div className="filter__item">
+          <label htmlFor="sort-by">Sort By</label>
+          <select
+            id="sort-by"
+            tabIndex="0"
+            aria-label="Sort By"
+            onChange={sortItems}
+          >
+            <option value="0" className="subject-option">
+              Description(A - Z)
+            </option>
+            <option value="1" className="subject-option">
+              Description(Z - A)
+            </option>
+            <option value="2" className="subject-option">
+              Brand(A - Z)
+            </option>
+            <option value="3" className="subject-option">
+              Brand(Z - A)
+            </option>
+            <option value="4" className="subject-option">
+              Price(Low - High)
+            </option>
+            <option value="5" className="subject-option">
+              Price(High - Low)
+            </option>
+          </select>
+        </div>
+      </div>
+
       <main id="items" className="container">
         {Items.map((item) => {
           const {
@@ -74,7 +123,8 @@ const Main = ({ Products, onClick }) => {
                 <h2 className="card__title">{`${caption}`}</h2>
               </a>
               <p className="card__price">
-                ${currency}${price}
+                {currency}
+                {price}
               </p>
               <button
                 onClick={() =>
