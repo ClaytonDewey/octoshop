1ce6419e4150ac9b398a2af4875954fca08357e2
ClaytonDewey <clay@dryadmedia.com>
2022-08-13 09:13:19 -0500

    Conditionally display cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,27 +3,31 @@ import React from "react";
 const ShoppingCart = ({ itemsInCart, onClick }) => {
   return (
     <>
-      {itemsInCart.map((item) => {
-        return (
-          <div key={item.prodId} className="cart__item">
-            <button
-              onClick={() => onClick(item.prodId)}
-              className="btn btn__delete"
-            >
-              -
-            </button>
-            <img
-              className="cart__img"
-              src={item.mediumImageURL}
-              alt={item.caption}
-            />
-            <span className="cart__price">
-              {item.currency}
-              {item.price}
-            </span>
-          </div>
-        );
-      })}
+      {itemsInCart.length ? (
+        itemsInCart.map((item) => {
+          return (
+            <div key={item.prodId} className="cart__item">
+              <button
+                onClick={() => onClick(item.prodId)}
+                className="btn btn__delete"
+              >
+                -
+              </button>
+              <img
+                className="cart__img"
+                src={item.mediumImageURL}
+                alt={item.caption}
+              />
+              <span className="cart__price">
+                {item.currency}
+                {item.price}
+              </span>
+            </div>
+          );
+        })
+      ) : (
+        <h2>Your Cart is Empty</h2>
+      )}
     </>
   );
 };
