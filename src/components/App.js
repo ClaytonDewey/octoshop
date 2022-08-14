8f86546b2bc3ccab0697c03819af74eb7699e40f
ClaytonDewey <clay@dryadmedia.com>
2022-08-13 21:13:21 -0500

    Move sort function to Main.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import "./App.scss";
 const App = () => {
   const [itemsInCart, setItemsInCart] = useState([]);
   const cartCount = itemsInCart.length;
-  const [sortedItems, setSortedItems] = useState([].concat(Products.List));
+  const Items = Products.List;
 
   useEffect(() => {
     const items = window.localStorage.getItem("cart");
@@ -21,53 +21,6 @@ const App = () => {
       window.localStorage.setItem("cart", JSON.stringify(itemsInCart));
     }
   }, [itemsInCart]);
-
-  useEffect(() => {}, [sortedItems]);
-
-  const sortItems = (e) => {
-    const { value } = e.target;
-
-    switch (value) {
-      case 1:
-        const sortedZA = [...sortedItems].sort((a, b) =>
-          a.caption < b.caption ? 1 : -1
-        );
-        console.log(value, sortedZA);
-        setSortedItems(sortedZA);
-        break;
-      case 2:
-        const sortedBrandAZ = sortedItems.sort((a, b) =>
-          a.brand > b.brand ? 1 : -1
-        );
-        setSortedItems(sortedBrandAZ);
-        console.log(value, sortedItems);
-        break;
-      case 3:
-        const sortedBrandZA = sortedItems.sort((a, b) =>
-          a.brand < b.brand ? 1 : -1
-        );
-        setSortedItems(sortedBrandZA);
-        console.log(value, sortedItems);
-        break;
-      case 4:
-        const sortedLowHi = sortedItems.sort((a, b) =>
-          a.price > b.price ? 1 : -1
-        );
-        setSortedItems(sortedLowHi);
-        console.log(value, sortedItems);
-        break;
-      case 5:
-        const sortedHiLow = sortedItems.sort((a, b) =>
-          a.price < b.price ? 1 : -1
-        );
-        setSortedItems(sortedHiLow);
-        console.log(value, sortedItems);
-        break;
-      default:
-        console.log(value, sortedItems);
-        return sortedItems;
-    }
-  };
 
   const addToCart = (prodId, mediumImageURL, currency, price, caption) => {
     const itemToAdd = {
@@ -82,7 +35,6 @@ const App = () => {
 
   const removeFromCart = (prodId) => {
     const updatedCart = itemsInCart.filter((item) => item.prodId !== prodId);
-    console.log(updatedCart);
     setItemsInCart(updatedCart);
   };
 
@@ -93,11 +45,7 @@ const App = () => {
         onClick={removeFromCart}
         cartCount={cartCount}
       />
-      <Main
-        sortedItems={sortedItems}
-        onClick={addToCart}
-        sortItems={sortItems}
-      />
+      <Main Items={Items} onClick={addToCart} />
       <Footer />
     </>
   );
