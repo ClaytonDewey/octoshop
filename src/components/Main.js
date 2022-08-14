7b1af2715b4de6865c239c05863aef00257662c1
ClaytonDewey <clay@dryadmedia.com>
2022-08-13 21:13:54 -0500

    Get sort function working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "./Modal";
 
-const Main = ({ sortedItems, onClick, sortItems }) => {
+const Main = ({ Items, onClick }) => {
   const [isOpen, setOpen] = useState("false");
   const [selectedItem, setSelectedItem] = useState({});
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState("descAZ");
 
+  useEffect(() => {
+    const sortArray = (type) => {
+      let sorted;
+      const types = {
+        descAZ: "caption",
+        descZA: "caption",
+        brandAZ: "brand",
+        brandZA: "brand",
+        priceAZ: "price",
+        priceZA: "price",
+      };
+
+      const orders = {
+        descAZ: "forward",
+        descZA: "reverse",
+        brandAZ: "forward",
+        brandZA: "reverse",
+        priceAZ: "forward",
+        priceZA: "reverse",
+      };
+      const sortProperty = types[type];
+      const sortOrder = orders[type];
+
+      if (sortOrder === "forward") {
+        sorted = [...Items].sort((a, b) =>
+          a[sortProperty] > b[sortProperty] ? 1 : -1
+        );
+      } else {
+        sorted = [...Items].sort((a, b) =>
+          b[sortProperty] > a[sortProperty] ? 1 : -1
+        );
+      }
+      setData(sorted);
+    };
+
+    sortArray(sortType);
+  }, [sortType, Items]);
   const renderContent = () => {
     return (
       <>
@@ -13,12 +52,17 @@ const Main = ({ sortedItems, onClick, sortItems }) => {
         </div>
         <div className="modal__content-text">
           <p dangerouslySetInnerHTML={{ __html: selectedItem.description }}></p>
-          <p>{selectedItem.brand}</p>
-          <p>Item: {selectedItem.prodId}</p>
           <p>
-            {selectedItem.currency}
+            <strong>Brand:</strong> {selectedItem.brand}
+          </p>
+          <p>
+            <strong>Item#:</strong> {selectedItem.prodId}
+          </p>
+          <p>
+            <strong>Price:</strong> {selectedItem.currency}
             {selectedItem.price}
           </p>
+          <a href={selectedItem.productUrl}>Read More</a>
         </div>
       </>
     );
@@ -62,23 +106,23 @@ const Main = ({ sortedItems, onClick, sortItems }) => {
         <div className="filter__item">
           <label htmlFor="sort-by">Sort By</label>
           <select
+            onChange={(e) => setSortType(e.target.value)}
             id="sort-by"
             tabIndex="0"
             aria-label="Sort By"
-            onChange={sortItems}
           >
-            <option value="0">Description(A - Z)</option>
-            <option value="1">Description(Z - A)</option>
-            <option value="2">Brand(A - Z)</option>
-            <option value="3">Brand(Z - A)</option>
-            <option value="4">Price(Low - High)</option>
-            <option value="5">Price(High - Low)</option>
+            <option value="descAZ">Description (A - Z)</option>
+            <option value="descZA">Description (Z - A)</option>
+            <option value="brandAZ">Brand (A - Z)</option>
+            <option value="brandZA">Brand (Z - A)</option>
+            <option value="priceAZ">Price (Low - High)</option>
+            <option value="priceZA">Price (High - Low)</option>
           </select>
         </div>
       </div>
 
       <main id="items" className="container">
-        {sortedItems.map((item) => {
+        {data.map((item) => {
           const {
             prodId,
             brand,
